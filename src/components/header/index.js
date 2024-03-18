7b2f281f1426a6191f866b480a2f4bb1660aaa07
maalpi <mateus.mateus30@gmail.com>
2024-03-18 16:12:11 -0300

    Router DOM e Header

---
+++ src/components/header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import { Nav } from './styled';
+
+export default function Header() {
+  return (
+    <Nav>
+      <Link to="/" aria-label="Página inicial">
+        <FaHome size={24} />
+      </Link>
+      <Link to="/login" aria-label="Entrar">
+        <FaSignInAlt size={24} />
+      </Link>
+      <Link to="/perfil" aria-label="Perfil">
+        <FaUserAlt size={24} />
+      </Link>
+    </Nav>
+  );
+}
