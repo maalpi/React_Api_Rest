f01b026a74bf2678aba9a64e94b61dcfa715f092
maalpi <mateus.mateus30@gmail.com>
2024-03-24 22:36:16 -0300

    consumindo API

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoClicado = useSelector((state) => state.example.botaoClicado);
   return (
     <Nav>
       <Link to="/" aria-label="Página inicial">
         <FaHome size={24} />
       </Link>
-      <Link to="/login" aria-label="Entrar">
+      <Link to="/register" aria-label="Entrar">
         <FaSignInAlt size={24} />
       </Link>
-      <Link to="/perfil" aria-label="Perfil">
+      <Link to="/login" aria-label="Perfil">
         <FaUserAlt size={24} />
       </Link>
-      {botaoClicado ? 'Clicado' : 'Não Clicado'}
     </Nav>
   );
 }
